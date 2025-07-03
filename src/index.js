b6b2684c5d36f45d479e8d6f0d64b7bec42de9fe
Shubham Kushwaha <shubhamkushwaha2536@gmail.com>
2025-07-03 15:25:42 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+console.log(" hii there")
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("Mongo db connection failed", error);
+})
+
