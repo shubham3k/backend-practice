ebe23e0ed7d1004aa0a394a8c1793b9ff2dba440
Shubham Kushwaha <shubhamkushwaha2536@gmail.com>
2025-07-05 07:46:50 +0530

    api error handling

--- src/app.js
+++ src/app.js
@@ -16,4 +16,17 @@ app.use(express.urlencoded({extended : true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser)
 
+
+// routes import 
+
+import userRouter from './routes/user.routes.js'
+
+
+// routes decleration 
+// since things are seperated we use app.use to use middleware
+
+app.use("/users", userRouter)
+
+
+
 export { app } 
